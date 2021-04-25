482621b04ad74e69c1dc283fe3f05dc3cfaef1c4
sthu <shutinghu2000@gmail.com>
2021-04-25 17:53:57 -0600

    footer, diy icons, will be redesigned accordion stuff

---
+++ js/accordion-view.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+
+    $('.accordion li').click(function () {
+
+        var text = $(this).children('div');
+
+        if (text.is(':hidden')) {
+            text.slideDown('200');
+            $(this).children('span').html('-');
+        } else {
+            text.slideUp('200');
+            $(this).children('span').html('+');
+        }
+
+    });
+
+});
