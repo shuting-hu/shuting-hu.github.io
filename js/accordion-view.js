bd0b1eebb2fd5bb75c1bfcfebad20979ca11e234
sthu <shutinghu2000@gmail.com>
2021-05-10 00:45:12 -0600

    initial chatbot, added projects

--- js/accordion-view.js
+++ js/accordion-view.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
 
     $('.accordion li').click(function () {
 
-        var text = $(this).children('div');
+        var text = $(this).children('.hidden');
 
         if (text.is(':hidden')) {
             text.slideDown('200');
